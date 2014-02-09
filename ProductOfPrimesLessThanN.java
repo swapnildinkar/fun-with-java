ef34e0179929bb7e83a24b3b8dba1534d9bb5d1d
swapnildinkar <swapnildinkar@gmail.com>
2014-02-09 01:44:43 -0500

    Optimizations

--- ProductOfPrimesLessThanN.java
+++ ProductOfPrimesLessThanN.java
@@ -2,7 +2,6 @@
  * Program to calculate the pairs of Prime Numbers whose product is less than 'N'.
  */
 
-
 package learn;
 
 import java.io.BufferedReader;
@@ -14,58 +13,41 @@ public class ProductOfPrimesLessThanN {
 	public static void main(String[] args) throws IOException {
 		ProductOfPrimesLessThanN p = new ProductOfPrimesLessThanN();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter 'N': ");
 		p.findProductOfPrimesLessThanN(Integer.parseInt(br.readLine()));
 
 	}
 
-	//Calculate all Prime numbers less than 'n'.
-	int[] findPrimes(int n)
-	{
+	// Calculate all Prime numbers less than 'n'.
+	int[] findPrimes(int n) {
 		int primes[] = new int[n];
 		boolean b[] = new boolean[(n)];
-		int j = 1;
-		for(int i = 0 ;i < b.length; i++)
-		{
-			b[i] = false;
+		int j = 1, k = 1;
+		for (int i = 0; i < b.length; i++) {
+			b[i] = true;
 		}
-		b[0] = true;
-		b[0] = true;
-		
-		for(int i = 2; i < (n); i++)
-		{
-			j=2;
-			while(true)
-			{
-				if(i*j < (n))
-					b[i*(j++)] = true;
-				else
-					break;
-			}			
+		for (int i = 2; i < n; i++) {
+			if (b[i] == true) {
+				for (j = i*i; j < n; j += i) {
+					b[j] = false;
+				}
+				primes[k++] = i;
+			}
 		}
-		j=0;
-		
-		for(int i = 2;i < b.length; i++)
-		{
-			if(b[i] == false)
-			{
-				primes[j++] = i;
-			}	
-		}
+		j = 0;
 		return primes;
 	}
 
 	// Find prime number pairs whose product is less than 'n'.
 	void findProductOfPrimesLessThanN(int n) {
-		
-		int primes[] = findPrimes(n/2);
-		System.out.println("The prime number pairs with product less than " + n + " are: ");
-		for(int i = primes.length - 1; i>0; i--)
-		{
-			for(int j=0; j<i ; j++)
-			{
-				if((primes[i] * primes[j]) < n)
-				{
-					if(primes[i] == 0 || primes[j] == 0)
+
+		int primes[] = findPrimes(n / 2);
+		System.out.println("The prime number pairs with product less than " + n
+				+ " are: ");
+		for (int i = primes.length - 1; i > 0; i--) {
+			for (int j = 0; j < i; j++) {
+				if ((primes[i] * primes[j]) < n) {
+					if (primes[i] == 0 || primes[j] == 0)
 						continue;
 					System.out.println(primes[i] + "," + primes[j]);
 				}
